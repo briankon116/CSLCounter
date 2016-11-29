9725a89cbb7e63295d5c1a66a2c716c8c6e1d085
Brian Kondracki <briankondracki@Brians-MacBook-Air-2.local>
2016-11-29 17:01:31 -0500

    Added a shebang line and added the executable permission to the script file.

--- CSLCounter.py
+++ CSLCounter.py
@@ -1,37 +1,44 @@
+#!/usr/bin/env python
+
 import xlrd, os
 
+def totalComplaints():
+    for file in os.listdir('.'):
+        if not file.endswith(".xls"):
+            continue
+        # Open the workbook
+        workbook = xlrd.open_workbook(file)
 
+        # Open the first sheet
+        worksheet = workbook.sheet_by_index(0)
 
-for file in os.listdir('.'):
-    if not file.endswith(".xls"):
-        continue
-    # Open the workbook
-    workbook = xlrd.open_workbook(file)
+        # The counter for the facebook page complaints
+        facebookComments = 0
 
-    # Open the first sheet
-    worksheet = workbook.sheet_by_index(0)
-    
-    # The counter for the facebook page complaints
-    facebookComments = 0
-    
-    # The counter for the website comments
-    websiteComments = 0
+        # The counter for the website comments
+        websiteComments = 0
 
-    # Loop over all of the sheets in the workbook
-    for i in range (0, len(workbook.sheet_names())):
-        # Open the current sheet
-        worksheet = workbook.sheet_by_index(i)
+        # Loop over all of the sheets in the workbook
+        for i in range (0, len(workbook.sheet_names())):
+            # Open the current sheet
+            worksheet = workbook.sheet_by_index(i)
+
+            row = 2
+            while(row < worksheet.nrows):
+                # The current cell
+                cellValue = worksheet.cell(row,2).value
+                if('facebook' in (cellValue.lower())):
+                    facebookComments+=1
+                elif('website' in (cellValue.lower())):
+                    websiteComments+=1
+                row+=1
+
+        # Once the entire workbook has been searched, print out the values of the facebook comments and website comments
+        print "Facebook Complaints: {}".format(facebookComments)
+        print "Website Complaints: {}".format(websiteComments)
         
-        row = 2
-        while(row < worksheet.nrows):
-            # The current cell
-            cellValue = worksheet.cell(row,2).value
-            if('facebook' in (cellValue.lower())):
-                facebookComments+=1
-            elif('website' in (cellValue.lower())):
-                websiteComments+=1
-            row+=1
+def main():
+    totalComplaints()
+
     
-    # Once the entire workbook has been searched, print out the values of the facebook comments and website comments
-    print "Facebook Complaints: {}".format(facebookComments)
-    print "Website Complaints: {}".format(websiteComments)
+main()
